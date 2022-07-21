68b9355385bcc344ea11f73bd6e07df903024e6a
Fer <sombondrolo@gmail.com>
2022-07-21 19:01:15 -0300

    Fixed webDriver typo in capybara.rb

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -11,7 +11,7 @@ Capybara.register_driver :headless_selenium_chrome_in_container do |app|
   Capybara::Selenium::Driver.new app,
     browser: :remote,
     url: "http://selenium_chrome:4444/wd/hub",
-    desired_capabilities: Selenium::Webdriver::Remote::Capabilities.chrome(
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
       chromeOptions: { args: %w(headless disable-gpu) }
     )
 end
